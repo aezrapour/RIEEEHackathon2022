932016b63fb59424ad9068b47dc381dcacfa0df4
elmho01 <elmho01@gmail.com>
2022-03-27 11:02:29 -0400

    you are late

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -4,6 +4,7 @@
 
     <!-- Site Content == Between the <div> tags -->
 <div>
+  <h2 class = "subtitle"> Bolivia 2018 </h2>
   <b-carousel
     id="carousel-fade"
     style="text-shadow: 0px 0px 2px #000"
@@ -14,6 +15,29 @@
   >
     <b-carousel-slide
       caption="First slide"
+      img-src="https://picsum.photos/1024/480/?image=10"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Nulla vitae elit libero, a pharetra augue mollis interdum."
+      img-src="https://picsum.photos/1024/480/?image=52"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Third Slide"
+      img-src="https://picsum.photos/1024/480/?image=54"
+    ></b-carousel-slide>
+  </b-carousel>
+
+    <h2 class = 'subtitle'> Bolivia 2017 </h2>
+    <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+    img-width="1024"
+    img-height="480"
+  >
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
       img-src="https://picsum.photos/1024/480/?image=10"
     ></b-carousel-slide>
     <b-carousel-slide
@@ -58,5 +82,11 @@
 <style scoped>
 .push {
   height: 50px;
+}
+
+.subtitle {
+  padding-top: 35px;
+  padding-bottom: 20px;
+  text-align: center;
 }
 </style>
