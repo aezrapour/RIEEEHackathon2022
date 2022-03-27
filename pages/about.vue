1f1de39d96e9f9aca37f9015f5a702cc844a4584
aezrapour <86795185+aezrapour@users.noreply.github.com>
2022-03-27 10:43:39 -0400

    And Smell the Coffee

--- pages/about.vue
+++ pages/about.vue
@@ -1,32 +1,129 @@
 <template>
     <main>
-<div><NavBar /></div>
+<header><NavBar /></header>
     
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/pres.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
 
-<b-container profile class="p-4 bg-light">
-  <b-row>
-    <b-col>
-      <h2>Matthew Kolb</h2>
-      <h3>President</h3>
-      <b-img rounded="circle" id="Prez" thumbnail profile src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/pres.jpg" alt="Image 1"
-       width = "300"
-       length = "300"
-      >
-      
-      
-      </b-img>
-    </b-col>
-    <b-col>
-      <h2>Rishika Padnani</h2>
-      <h3>Vice President</h3>
-      <b-img rounded="circle" thumbnail profile src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/vicepres.jpg" alt="Image 2"
-       width = "300"
-       length = "300"
-      ></b-img>
-    </b-col>
-  </b-row>
-</b-container>
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/vicepres.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
 
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/projectlead.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="	https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/secretary.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+
+ <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/fund.jpg" 
+      height = 320
+      width = 200
+      alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/socialmedia.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/treasurer.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/events.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/webmaster.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/egcrep.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="push"></div>
+<Footer />
   </main>
 
 </template>
@@ -36,14 +133,66 @@ export default {
   name: 'AboutPage'
 }
 </script>
-
 <style scoped>
-div {
-    text-align: center;
+.push{
+  height: 50px;
 }
 
-img {
-  border: 10px solid rgb(0, 0, 0);
+/* Three columns side by side */
+.column {
+  float: left;
+  width: 50%;
+  margin-bottom: 20px;
+  padding: 0 16px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 22em
+}
+
+
+/* Display the columns below each other instead of side by side on small screens */
+@media screen and (max-width: 650px) {
+  .column {
+    width: 100%;
+    display: block;
+  }
+}
+
+/* Add some shadows to create a card effect */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+
+/* Some left and right padding inside the container */
+.container {
+  padding: 0 20px;
+}
+
+/* Clear floats */
+.container::after, .row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.title {
+  color: grey;
+}
+
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
 }
 
 </style>
