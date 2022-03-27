11ecbdb52c62fb982bf3d689c85216369da82c2c
elmho01 <elmho01@gmail.com>
2022-03-27 12:32:49 -0400

    Final

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -15,16 +15,101 @@
   >
     <b-carousel-slide
       caption=""
-      img-src="https://b2p.rutgers.edu/sites/default/files/gallery/photos%20/IMAG5372.jpg"
+      img-src= "../assets/Capiana/(46).jpg"
     ></b-carousel-slide>
     <b-carousel-slide
       caption=""
-      img-src="https://b2p.rutgers.edu/sites/default/files/gallery/photos%20/%2846%29.jpg"
+      img-src= "../assets/Capiana/(47).jpg"
     ></b-carousel-slide>
     <b-carousel-slide
       caption=""
-      img-src="https://b2p.rutgers.edu/sites/default/files/gallery/photos%20/%2862%29.jpg"
+      img-src= "../assets/Capiana/(48).jpg"
     ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(49).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(50).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(51).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(52).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(53).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(54).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(55).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(56).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(57).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(58).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(59).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(60).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(61).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(62).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(63).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(64).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(65).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(66).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(67).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(68).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption=""
+      img-src= "../assets/Capiana/(69).jpg"
+    ></b-carousel-slide>
+
   </b-carousel>
 
     <h2 class = 'subtitle'>Palmar Pampa B, Bolvia 2017</h2>
@@ -38,24 +123,85 @@
   >
     <b-carousel-slide
       caption="Palmar Pampa B, Bolivia 2017"
-      img-src="https://b2p.rutgers.edu/sites/default/files/gallery/photos%20/20170713_145524.jpg"
+      img-src="../assets/Palmar/Prep (1).jpg"
     ></b-carousel-slide>
     <b-carousel-slide
-      caption=""
-      img-src="https://b2p.rutgers.edu/sites/default/files/gallery/photos%20/20170818_084949.jpg"
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (2).jpg"
     ></b-carousel-slide>
     <b-carousel-slide
-      caption=""
-      img-src="https://b2p.rutgers.edu/sites/default/files/gallery/photos%20/20170819_100321.jpg"
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (3).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (4).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (5).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (6).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (7).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (8).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (9).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (10).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (11).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (12).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (13).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (14).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (15).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (16).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (17).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (18).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (19).jpg"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      caption="Palmar Pampa B, Bolivia 2017"
+      img-src="../assets/Palmar/Prep (20).jpg"
     ></b-carousel-slide>
   </b-carousel>
-
-  id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    indicators
-    img-width="1024"
-    img-height="480"
 </div>
 
 
