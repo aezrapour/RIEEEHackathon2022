bb5c8edc38bdb12de3968a5c211c66db4ddc0762
aezrapour <86795185+aezrapour@users.noreply.github.com>
2022-03-27 03:02:35 -0400

    Circle pt2

--- pages/about.vue
+++ pages/about.vue
@@ -27,70 +27,6 @@
   </b-row>
 </b-container>
 
-<<<<<<< Updated upstream
-
-
-
-
-    <h2>Matthew Kolb</h2>
-    <h3>President</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/pres.jpg"
-     class= "img-circle"
-     width = "400"
-     length = "400"
-    >
-
-    <h2>Rishika Padnani</h2>
-    <h3>Vice President</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/vicepres.jpg"  
-    width = "400"
-    length = "400"
-    >
-
-    <h2>Dan Boehm</h2>
-    <h3>Project Lead</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/projectlead.jpg" 
-     width = "400"
-     length = "400"
-    >
-    
-    <h2>Sydney Jenkins</h2>
-    <h3>Secretary</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/secretary.png" 
-     width = "400"
-     length = "400"
-    >
-
-    
-    <h2>Lea-May Cabigas</h2>
-    <h3>Fundraising Lead</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/fund.jpg" 
-     width = "400"
-     length = "400"
-    >
-    
-    <h2>Mariah Diaz</h2>
-    <h3>Social Media Chair</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/socialmedia.jpg" 
-     width = "400"
-     length = "400"
-    >
-    
-    
-    <h2>Kara Forest</h2>
-    <h3>Treasurer</h3>
-    <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/treasurer.jpg" 
-     alt="Avatar"
-     width = "400"
-     length = "400"
-    >
-    
-    
-<!-- Site footer -->
-<div class="push"></div>
-    <Footer />
-=======
->>>>>>> Stashed changes
   </main>
 
 </template>
@@ -100,12 +36,6 @@ export default {
   name: 'AboutPage'
 }
 </script>
-<<<<<<< Updated upstream
-<style scoped>
-.push {
-  height: 50px;
-}
-=======
 
 <style scoped>
 div {
@@ -116,5 +46,4 @@ img {
   border: 10px solid rgb(0, 0, 0);
 }
 
->>>>>>> Stashed changes
 </style>
