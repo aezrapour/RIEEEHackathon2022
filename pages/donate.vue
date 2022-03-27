c77553390d5b3a1bfb5ba046c656bea38c72853a
jcsolomon19 <jrs556@scarletmail.rutgers.edu>
2022-03-27 10:59:28 -0400

    hi

--- pages/donate.vue
+++ pages/donate.vue
@@ -1,59 +1,16 @@
 <template>
     <main>
-    <h1>Donate Page</h1>
-            <NuxtLink to="/">
-      Home
-    </NuxtLink>
-    <NuxtLink to="/about">
-      About
-    </NuxtLink>
-        <NuxtLink to="/event">
-      Events
-    </NuxtLink>
-        <NuxtLink to="/donate">
-      Donate
-    </NuxtLink>
-        <NuxtLink to="/gallery">
-      Gallery
-    </NuxtLink>
-      <p style = "font-family: Impact">This is text</p>
+    <div>
+      <NavBar/>
+      </div>
+  
      
 
 
+<img class= "mainPic" src = "../assets/typorama.jpg" style = "width:1400px;height:600px;align:center">
 
-<meta charset = "utf-8">
-<title>Untitled Document</title>
-
-
-
-<style>
-  *{margin:0; padding:0;}
-  body{font=size:20px;}
-  .banner{position:relative; width:100%;}
-  .banner img{width:90%;}
-  .heading{color:red; position: absolute; top: 50%; width:100%; text-align:center;}
-</style>
-
-<div class = "container">
-  <div class = "row">
-  <div class = "banner">
-  <img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/4.jpg">
-    <h2 class = "heading">Hellow How Are You!</h2>
-    </div>
-  </div>
-</div>
-
-
-
-
-     
-<p>hello my name is</p> 
-<img src = "https://b2p.rutgers.edu/sites/all/themes/nexus/images/4.jpg"
-width = "1500"
-length = "1000"
->
-
-
+<p></p> 
+<p></p> 
 <p></p> 
 <h1 style = "font-family: Impact">Are You Ready For This Challenge?</h1>
 
@@ -64,7 +21,7 @@ align = "right"
 width = "750"
 length = "1500"
 >
-
+<p></p> 
 <p>Engineers In Action is always on the look: For innovators. For engineers. For pioneers. If you think you are one of these, come join us in our journey. Attend our meetings and tournaments, help our Bridge Design Team, raise funds for our next project, or travel with us to Bolivia for our 2019 summer project!</p> 
 <p>This year in the summer of 2019, Engineers In Action will be traveling to Bolivia where we will be constructing our second footbridge. The cost required for this project will include expenses such as student travel, bridge materials, travel team training, and other essential costs. Thankfully our travel team will be larger this year however, this means our costs will also increase as well. If you wish to contribute to B2P and our mission you may donate directly to our fund through here and remember no donation is too small! We appreciate all your support and dedication to this cause, thank you.</p> 
 
@@ -82,5 +39,6 @@ length = "1500"
 export default {
   name: 'DonatePage'
 }
+
 </script>
 
