116252aa76ae3e2762fb39c590c17ae625010daa
aezrapour <86795185+aezrapour@users.noreply.github.com>
2022-10-29 13:55:39 -0400

    new e-board :)

--- pages/about.vue
+++ pages/about.vue
@@ -5,33 +5,33 @@
 <div class="row">
   <div class="column">
     <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/pres.jpg" alt="Jane" style="width:100%">
+      <img src= "../assets/Eboard/Sarah.JPG" alt="Sarah" style="width:100%">
       <div class="container">
-        <h2>Matthew Kolb</h2>
+        <h2>Sarah Mah</h2>
+        <p class="title">E-Board Advisor</p>
+        <p>Hi there! My name is Sarah Mah and I am a Junior studying Packaging Engineering with a minor in Fine Art. Currently, I'll be serving as EIA's Board Advisor where I help onboard new members and assist in tasks for organizing events/meetings. I joined EIA as a freshman because I was inspired by the organization's compassion and mission to make change by helping and connecting with communities. It's a rewarding experience to be a part of a caring and inclusive group! Outside of school, I enjoy spending my free time painting and listening to music. Can't wait for an amazing semester! :D </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="../assets/Eboard/Alex.JPG" alt="Alex" style="width:100%">
+      <div class="container">
+        <h2>Alex Sitarz</h2>
         <p class="title">President</p>
-        <p>"I joined Engineers In Action to make an impact in the global community while still developing engineering skills in school. This organization has enabled students such as myself to understand their influence as a global citizen, and as people who have a chance to change lives. I look forward to seeing the positive impact Rutgers Engineers In Action provides for both people around the world as well as the students who are involved."</p>
+        <p>Hi! My name is Alex and I'm a senior studying Civil Engineering. I am the President of Engineers in Action. In 2021 I traveled with EIA to build a vehicular bridge in West Virginia. I joined EIA my freshman year because I saw a great opportunity to help people while pursuing my passion in engineering. Outside of the classroom I love to cook, bake, and do arts and crafts. I'm looking forward to a great last year.</p>
       </div>
     </div>
   </div>
 
   <div class="column">
     <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/vicepres.jpg" alt="Mike" style="width:100%">
+      <img src="../assets/Eboard/Cole.JPG" alt="Cole" style="width:100%">
       <div class="container">
-        <h2>Rishika Padnani</h2>
+        <h2>Cole Jenkins</h2>
         <p class="title">Vice President</p>
-        <p>"The very first time I heard about Bridges to Prosperity (now Engineers in Action) in my freshman year, I was confident that it was something I was going to be a part of. This organization is extremely unique and noble in its purpose of allowing access to healthcare, education, and water to developing countries via pedestrian bridges, and I feel blessed to be a part of it. This past summer, I traveled to Bolivia to construct an 82-meter span bridge along with nine other university students and had an unforgettable experience. Now, I understand that EIA is a place in which I know I can make a difference with everything that I love: amazing people, service to others, and civil engineering.I am more than privileged to continue being a part of this fantastic e-Board as this year’s Vice President."</p>
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/projectlead.jpg" alt="John" style="width:100%">
-      <div class="container">
-        <h2>Dan Boehm</h2>
-        <p class="title">Project Lead</p>
-        <p>"The experience of working side by side with the community, eating with them and living with them and getting to see such a different way of life was life-changing. The lessons I learned about engineering and construction are just the beginning of what I've gained from EIA."</p>
+        <p>Hi all, my name is Cole Jenkins and I am a Junior studying Mechanical Engineering at the Rutgers School of Engineering. For this coming school year, I will be serving as the Vice President of Engineers in Action (EIA) where I have the opportunity to make a real difference in communities here and around the world! The biggest reason I joined this club was to be able to give back to others and have the reward of knowing I was able to make a big impact on the different communities we help. Outside of the engineering scope, I love hiking, skiing/snowboarding, and watching and playing a variety of sports. A final fun fact about me is my favorite show is Avatar: The Last Airbender, with my favorite character being Zuko! I look forward to seeing and working with you all this semester!</p>
       </div>
     </div>
   </div>
@@ -40,68 +40,67 @@
 <div class="row">
   <div class="column">
     <div class="card">
-      <img src="	https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/secretary.png" alt="Jane" style="width:100%">
+      <img src="../assets/Eboard/John.JPG" alt="John" style="width:100%">
       <div class="container">
-        <h2>Sydney Jenkins</h2>
-        <p class="title">Secretary</p>
-        <p>"Hi! My name is Sydney and I am the current secretary for Rutgers Engineers in Action. One of my favorite things about being involved in EIA is that, through the club, you are not only provided with the opportunity to make a positive impact on places that you would otherwise not be able to reach, but you also get access to an amazing family. Another thing I love is that I am able to apply the skills that I learn through classes on real-world applications, and also acquire new skills that I would otherwise not learn. If you have the chance, come check out to see what EIA is like all for yourself! Hope to see you soon!"</p>
+        <h2>John O’Keefe</h2>
+        <p class="title">Project Manager</p>
+        <p>Hello there! I am John O'Keefe and I am serving as the Project Manager this year. I am a junior majoring in civil engineering and this is my second year in the club after being a co-webmaster last year. I am excited to help lead the bridge design of our chapter and work closely with the travel members to ensure we build the best bridge we can. EIA has become my favorite part of college because it has allowed me to grow as a civil student, embrace new cultures and experiences, and do this all with some of the most intelligent and kindest people surrounding me. I cannot wait to see what we will accomplish this year.</p>
       </div>
     </div>
   </div>
 
  <div class="column">
     <div class="card">
-      <img src="" 
-      alt="John" style="width:100%">
+      <img src="../assets/Eboard/Amol.JPG" alt="Amol" style="width:100%">
       <div class="container">
-        <h2>Lea-May Cabigas</h2>
-        <p class="title">Fundraising Lead</p>
-        <p>"I'm Lea-May Cabigas and I am the Co-Fundraising Chair! I am a junior majoring in Civil Engineering. Over the summer, I was given the opportunity to take part of the tag-along team to Bolivia. I got involved with EIA with the interest of traveling and being able to improve the lives of a community in need by using what I've learned in classes and on the field."</p>
-      </div>
-    </div>
-  </div>
-
-  <div class="column">
-    <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/socialmedia.jpg" alt="John" style="width:100%">
-      <div class="container">
-        <h2>Mariah Diaz</h2>
-        <p class="title">Social Media Chair</p>
-        <p>"Hi! My name is Mariah Diaz, and I’m the Publicist / Social Media Chair for EIA. I am currently a junior in Civil and Environmental Engineering with a minor in Urban Planning and Design. I’ve been involved in R-DELLC since I was a freshman, promoting women in engineering, and I did research on restoring a local ecosystem for the UCWS Lab this past year. EIA was the first engineering club I ever heard of when I toured Rutgers. Learning about our direct efforts to build bridges in developing communities and having that opportunity to work in-country really inspired me, and it was definitely something that I wanted to be a part of since the beginning. "</p>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="row">
-  <div class="column">
-    <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/treasurer.jpg" alt="Jane" style="width:100%">
-      <div class="container">
-        <h2>Kara Forest</h2>
+        <h2>Amol Srivastava</h2>
         <p class="title">Treasurer</p>
-        <p class="par"> "Kara Forest has been part of EIA since Freshman year as the clubs' EGC Representative and is currently the Treasurer in her sophomore year. Kara Forest Majors in Environmental Science and is Minoring in Environmental Policy at the Rutgers University School of Environmental and Biological Sciences. She loves being outside, hiking, and hanging out with friends. What Kara loves most about EIA is that if building the bridge in-country or coordinating from home, all the work done supports the greater community with incalculable impact. "</p>
+        <p>Hey, my name is Amol Srivastava and I’m a sophomore studying Civil Engineering. For this school year I’ll be serving as EIA’s treasurer and working closely with our members to coordinate trip funding and fundraisers. I joined EIA because it is a great blend of technical engineering and humanitarian efforts. It’s also a great place to meet new people and learn more about what it takes to build a bridge. Outside of school, I like cycling, being outdoors and spending time with friends and family. I’m excited to meet all of you this semester!</p>
       </div>
     </div>
   </div>
 
   <div class="column">
     <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/events.jpg" alt="Mike" style="width:100%">
+      <img src="../assets/Eboard/Eric.JPG" alt="Eric" style="width:100%">
       <div class="container">
-        <h2>Morty Hagh</h2>
-        <p class="title">Events Lead</p>
-        <p>"I am a junior majoring in Civil Engineering and have been a member of EIA since Fall 2017. Initially, I joined this organization to gain a sense of how engineers can help communities that are in need. But now that I have been working with such talented and passionate engineers for two years, I feel that I have been involved in a culture that seeks to engineer a better world. Here at EIA, we aim to provide remote villages in developing countries a convenient access to the outside world. Our bridges provide a safe path for children to go to school, give convenient access to nearby medical facilities, and enable farmers to sell their goods at local markets. I am proud to be a part of such a group that aims to leave lasting impact globally!"</p>
+        <h2>Eric Rickershauser</h2>
+        <p class="title">Secretary</p>
+        <p>My name is Eric Rickershauser, and I’m the Secretary for Rutgers EIA. I am currently a junior in Packaging Engineering with a minor in Business Administration. As a member of EIA, I have not only been part of an amazing community of people but I've had the chance to be part of a group that helps so many people and makes a real difference in the world.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="../assets/Eboard/Jonathan.JPG" alt="Jonathan" style="width:100%">
+      <div class="container">
+        <h2>Jonathan Liu</h2>
+        <p class="title">Events Coordinator</p>
+        <p class="par"> Hello, I’m Jonathan Liu, a sophomore majoring in civil engineering. I’m the events coordinator for EIA, so I’ll help schedule fun activities like movie nights or fundraising events that help our club. I joined this club because I love to travel, and the main goal of helping communities in need was something that I could get behind. In my free time I spend a lot of time watching YouTube, and I grew up on Nintendo consoles.</p>
       </div>
     </div>
   </div>
 
   <div class="column">
     <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/webmaster.jpg" alt="John" style="width:100%">
+      <img src= "../assets/Eboard/Sam-Defalco.JPG" alt="SamD" style="width:100%">
       <div class="container">
-        <h2>Mona Randive</h2>
+        <h2>Sam DeFalco</h2>
+        <p class="title">Flyerboy</p>
+        <p>Hello, my name is Sam DeFalco and I’m a Junior majoring in Civil Engineering. This year I’ll be serving as EIA’s Flyerboy and informing everyone of the planned events. I joined EIA because it seemed fun and a great place to meet new people. When not studying, I like hiking, and playing video games. I'm excited to meet all of you!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src= "../assets/Eboard/Rohan.JPG" alt="Rohan" style="width:100%">
+      <div class="container">
+        <h2>Rohan Rahalkar</h2>
         <p class="title">Webmaster</p>
-        <p>"My name is Mona Ranadive and I am studying Industrial and Systems Engineering. EIA was the first engineering club that I joined and it provided me with a close knit group of individuals. I love being a part of club who aims to help others and have also learned a lot about bridge design."</p>
+        <p>Hello! My name is Rohan Rahalkar and I’m a senior majoring in Computer Engineering with a minor in Computer Science. This year, I will be managing all digital content that we post on the EIA website. I have been working with IEEE to build a new website and I hope our website highlights some of the amazing work our organization has been involved in this past year. I joined EIA last year to meet new people and to grow my skills as an engineer. We did a fun activity where our members and I used popsicle sticks to build load-bearing bridges! Some fun facts about me: I love chocolate ice cream, biking outdoors, listening to good music, and finding new shows to watch on Netflix. I can’t wait for another awesome year with all of you!</p>
       </div>
     </div>
   </div>
@@ -110,14 +109,50 @@
 <div class="row">
   <div class="column">
     <div class="card">
-      <img src="https://b2p.rutgers.edu/sites/all/themes/nexus/images/eboardpics/egcrep.png" alt="Jane" style="width:100%">
+      <img src= "../assets/Eboard/Jessica.JPG" alt="Jessica" style="width:100%">
       <div class="container">
-        <h2>Alexandra Sitarz</h2>
-        <p class="title">ECG Representative</p>
-        <p>"I joined EIA because I'venpm always been passionate about helping people. I knew I wanted to be in a club that makes a visible difference in people's lives and EIA gives me the opportunity to combine my major and my passion for helping people into one. "</p>
+        <h2>Jessica Youssef</h2>
+        <p class="title">Social Media</p>
+        <p>Hey! My name is Jessica Youssef and I’m a sophomore studying Civil Engineering. I’ll be taking care of EIA’s social media by posting reminders of upcoming events and updating with photos of memorable moments. I joined EIA because it provides real world experience that I lack from classes. I also get to meet people with the same passion and goals as me which gives me a sense of community. Some cool facts: I can fluently speak Arabic, I made everyone think I was from London the first day of school and I like to play bball. I’m excited for the new school year and can’t wait to see you all!</p>
       </div>
     </div>
   </div>
+
+  <div class="column">
+    <div class="card">
+      <img src= "../assets/Eboard/Ali.JPG" alt="Ali" style="width:100%">
+      <div class="container">
+        <h2>Ali Sozen</h2>
+        <p class="title">Fundraising Chair</p>
+        <p>Hi, my name is Ali Sozen, I am a sophomore majoring in Civil Engineering, and I am the Fundraising Lead for EIA this school year, I will be leading the fundraising events for EIA. I joined EIA as I wanted to help communities in need while gaining engineering experience. My hobbies include hiking, hanging out with friends, and working out.</p>
+      </div>
+    </div>
+  </div>
+
+    <div class="column">
+    <div class="card">
+      <img src= "../assets/Eboard/Grace.JPG" alt="Grace" style="width:100%">
+      <div class="container">
+        <h2>Grace Morris</h2>
+        <p class="title">EGC Representative</p>
+        <p>Hey! My name is Grace Morris and I'm a junior studying Civil Engineering. As the EGC Representative, I relay important information back and forth from our Engineering Government. I joined EIA as a way of connecting with others and gaining fulfilling experiences. As a transfer student, this club offers me a chance to meet with other people in my major, and a way of gaining real-world experience while helping others less fortunate. Some fun facts about me are that I can draw realism, I play volleyball, and I love Vietnamese food! I can't wait for what this upcoming year brings!</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src= "../assets/Eboard/SamLee.JPG" alt="Sam Lee" style="width:100%">
+      <div class="container">
+        <h2>Sam Lee</h2>
+        <p class="title">Travel Advisor</p>
+        <p>Hello! My name is Samuel Lee and I am a senior studying civil engineering and urban planning and design. I will be a travel advisor for the Rutgers EIA chapter where I use my travel experience from the 2022 Emabheleni bridge built in Eswatini to help prepare new travelers for their upcoming travel experience. I joined EIA because as a civil engineering student, I was inspired by their mission of connecting isolated communities to essential resources in an effort to further develop sustainable infrastructure and economic prosperity. Some fun facts about me: I can play the piano and I am also an e-board member of the Rutgers Running Club. I look forward to meeting and working with all of our new members!</p>
+      </div>
+    </div>
+  </div>
+
 </div>
 
 <div class="push"></div>
---
+++ pages/event.vue
@@ -0,0 +1,27 @@
+<template>
+<main>
+    <NavBar />
+
+    
+    <!-- Site Content == Between the <div> tags -->
+<div class="main-content">
+<p>Hello World</p>
+<p>How is the World?</p>
+</div>
+
+    <!-- Site footer -->
+<div class="push"></div>
+    <Footer />
+</main>
+</template>
+
+<script>
+export default {
+  name: 'EventPage'
+}
+</script>
+<style scoped>
+.push {
+  height: 50px;
+}
+</style>
